f7f654eaac0ebc8b3822e1975d69f3dbf58d8d71
sekercih <67022880+sekercih@users.noreply.github.com>
2022-09-07 21:51:02 +0300

    AppiumStartUdemy

---
+++ src/test/java/appiuTest/emulator_real_device/C01_FirstTest.java
@@ -0,0 +1,35 @@
+package appiuTest.emulator_real_device;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class C01_FirstTest {
+    @Test
+    public void test1() throws MalformedURLException {
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+        //todo adb shell getprop ro.build.version.release
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"7.0");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+        //todo emulator -list-avds
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel2");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\Mehmet\\Desktop\\com.AppiumStart\\src\\test\\app\\Duolingo.apk");
+
+        AndroidDriver<AndroidElement> driver=new
+                AndroidDriver<>(new URL("http:127.0.0.1:4723/wd/hub"),capabilities);
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+
+
+
+
+
+
+    }
+}
